af9fd3c021c1c49f5857619a5793dde84859b65f
TheCubiq <jlajsek@gmail.com>
2023-07-02 04:58:26 +0200

    📁 Folder Component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,38 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-    Visit 
-    <a href="https://kit.svelte.dev">kit.svelte.dev</a> 
-    to read the documentation
-</p>
+<script>
+	import Folder from "./folder.svelte";
+
+    let langs = [
+        { name: "js", icon: "js" },
+        { name: "ts", icon: "ts" },
+        { name: "svelte", icon: "svelte" },
+        { name: "stylus", icon: "stylus" },
+        { name: "html", icon: "html"}
+    ]
+
+    let toggled = false
+</script>
+
+<div class="contentWrapper">
+    <Folder>
+        {#each langs as lang}
+            <div class="file">{lang.icon}</div>
+        {/each}
+    </Folder>
+</div>
+
+<style lang="stylus" type="text/stylus">    
+
+
+    .contentWrapper
+        display flex
+        position relative
+        height 100%
+        // width 100%
+        color #fff
+        font-family 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif
+        
+        padding-left 10%
+
+        justify-content center
+        align-items center
+</style>
---
+++ src/routes/folder.svelte
@@ -0,0 +1,33 @@
+<script>
+	import { createEventDispatcher, onMount } from 'svelte';
+	import { afterUpdate } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+
+	let isOpen = false;
+
+	function toggleFolder() {
+		isOpen = !isOpen;
+		dispatch('toggle', isOpen);
+	}
+
+	
+</script>
+
+<div aria-hidden="true" class="folder" class:isOpen id="folder" on:click={toggleFolder}>
+	<div class="folder-icon">📁</div>
+	<div class="folder-name">Folder</div>
+</div>
+
+{#if isOpen}
+	<div class="file-list">
+		<slot />
+	</div>
+{/if}
+
+<style lang="stylus" type="text/stylus">
+	itemColor = hsl(212, 75%, 58%)
+	// @import '../styles/folder.styl'
+	@require '../styles/folder.styl'
+	
+</style>
